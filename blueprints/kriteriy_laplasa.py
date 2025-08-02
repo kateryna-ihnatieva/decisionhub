dd61163652b2c8d0d3f6ca96a42ba088559792da
kateryna-ihnatieva <ihnatieva.kateryna@gmail.com>
2025-08-02 15:29:41 +0300

    ✨Created page with names for Laplasa

--- blueprints/kriteriy_laplasa.py
+++ blueprints/kriteriy_laplasa.py
@@ -18,3 +18,26 @@ def index():
         "name": current_user.get_name() if current_user.is_authenticated else None,
     }
     return render_template("Laplasa/index.html", **context)
+
+
+@kriteriy_laplasa_bp.route("/names", methods=["GET", "POST"])
+def names():
+    num_alt = int(request.args.get("num_alt"))
+    num_conditions = int(request.args.get("num_conditions"))
+    laplasa_task = (
+        request.args.get("laplasa_task") if request.args.get("laplasa_task") else None
+    )
+
+    # Збереження змінної у сесії
+    session["num_alt"] = num_alt
+    session["num_conditions"] = num_conditions
+    session["laplasa_task"] = laplasa_task
+
+    context = {
+        "title": "Імена",
+        "num_alt": num_alt,
+        "num_conditions": num_conditions,
+        "name": current_user.get_name() if current_user.is_authenticated else None,
+    }
+
+    return render_template("Laplasa/names.html", **context)
