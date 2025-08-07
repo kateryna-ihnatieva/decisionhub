208358bc61db7b43f4cbd409b44e387df5f7d930
kateryna-ihnatieva <ihnatieva.kateryna@gmail.com>
2025-08-07 23:58:24 +0300

    ✨Updated tree for mai

--- mymodules/mai.py
+++ mymodules/mai.py
@@ -7,7 +7,7 @@ from graphviz import Digraph
 def do_matrix(krit=0, matrix=0, criteria=0, num_alt=0):
     def process_matrix_element(matr, i, j):
         value = matr[j][i]
-        if '/' in value:
+        if "/" in value:
             frac = Fraction(value)
             matr[i][j] = str(frac.denominator)
         else:
@@ -16,7 +16,7 @@ def do_matrix(krit=0, matrix=0, criteria=0, num_alt=0):
 
     matr = []
     if krit:
-        matr = [matrix[i:i + criteria] for i in range(0, criteria ** 2, criteria)]
+        matr = [matrix[i : i + criteria] for i in range(0, criteria**2, criteria)]
 
         for i in range(len(matr)):
             for j in range(len(matr[i])):
@@ -24,8 +24,10 @@ def do_matrix(krit=0, matrix=0, criteria=0, num_alt=0):
                     process_matrix_element(matr, i, j)
     else:
         for i in range(criteria):
-            matr.append([matrix[j: j + num_alt] for j in range(0, num_alt ** 2, num_alt)])
-            matrix = matrix[num_alt ** 2:]
+            matr.append(
+                [matrix[j : j + num_alt] for j in range(0, num_alt**2, num_alt)]
+            )
+            matrix = matrix[num_alt**2 :]
 
         # Змінюємо елементи нижньої трикутної матриці
         for m in matr:
@@ -63,7 +65,9 @@ def do_norm_vector(krit=0, comp_vector=0, criteria=0, num_alt=0):
         norm_vector = [comp_vector[c] / sum(comp_vector) for c in range(criteria)]
     else:
         for i in range(criteria):
-            norm_vector[i] = [comp_vector[i][c] / sum(comp_vector[i]) for c in range(num_alt)]
+            norm_vector[i] = [
+                comp_vector[i][c] / sum(comp_vector[i]) for c in range(num_alt)
+            ]
 
     return norm_vector
 
@@ -79,7 +83,10 @@ def do_sum_col(krit=0, matr=0, criteria=0, num_alt=0):
             sum_col.append(s)
     else:
         for c in range(criteria):
-            sum_col[c] = [sum([eval(matr[c][j][i]) for j in range(num_alt)]) for i in range(num_alt)]
+            sum_col[c] = [
+                sum([eval(matr[c][j][i]) for j in range(num_alt)])
+                for i in range(num_alt)
+            ]
 
     return sum_col
 
@@ -112,16 +119,16 @@ def do_l_max(krit=0, prod_col=0, criteria=0):
 def do_consistency(krit=0, l_max=0, criteria=0, num_alt=0):
     # Випадкова узгодженість
     random_consistency = {
-        '1': 0,
-        '2': 0,
-        '3': 0.58,
-        '4': 0.9,
-        '5': 1.12,
-        '6': 1.24,
-        '7': 1.32,
-        '8': 1.41,
-        '9': 1.45,
-        '10': 1.49
+        "1": 0,
+        "2": 0,
+        "3": 0.58,
+        "4": 0.9,
+        "5": 1.12,
+        "6": 1.24,
+        "7": 1.32,
+        "8": 1.41,
+        "9": 1.45,
+        "10": 1.49,
     }
     index_consistency = [] if krit else [[] for _ in range(criteria)]
     relation_consistency = [] if krit else [[] for _ in range(criteria)]
@@ -129,7 +136,8 @@ def do_consistency(krit=0, l_max=0, criteria=0, num_alt=0):
         if criteria >= 3:
             index_consistency.append((l_max[0] - criteria) / (criteria - 1))
             relation_consistency.append(
-                (index_consistency[0] / (random_consistency[str(criteria)])) * 100)
+                (index_consistency[0] / (random_consistency[str(criteria)])) * 100
+            )
         else:
             index_consistency.append(0)
             relation_consistency.append(0)
@@ -139,7 +147,8 @@ def do_consistency(krit=0, l_max=0, criteria=0, num_alt=0):
             for c in range(criteria):
                 index_consistency[c].append((l_max[c][0] - num_alt) / (num_alt - 1))
                 relation_consistency[c].append(
-                    (index_consistency[c][0] / (random_consistency[str(num_alt)])) * 100)
+                    (index_consistency[c][0] / (random_consistency[str(num_alt)])) * 100
+                )
         else:
             for c in range(criteria):
                 index_consistency[c].append(0)
@@ -158,8 +167,9 @@ def do_lst_norm_vector(krit=0, name=0, criteria=0, norm_vector=0, num_alt=0, g=0
 
         # сортування списку
 
-        lst_norm_vector[0] = sorted(lst_norm_vector[0].items(),
-                                    key=operator.itemgetter(1), reverse=True)
+        lst_norm_vector[0] = sorted(
+            lst_norm_vector[0].items(), key=operator.itemgetter(1), reverse=True
+        )
         lst_norm_vector[0] = {k: v for k, v in lst_norm_vector[0]}
 
     elif g == 1:
@@ -167,7 +177,9 @@ def do_lst_norm_vector(krit=0, name=0, criteria=0, norm_vector=0, num_alt=0, g=0
         for i in range(num_alt):
             lst_norm_vector[0][name[i]] = norm_vector[i]
 
-        lst_norm_vector[0] = sorted(lst_norm_vector[0].items(), key=operator.itemgetter(1), reverse=True)
+        lst_norm_vector[0] = sorted(
+            lst_norm_vector[0].items(), key=operator.itemgetter(1), reverse=True
+        )
         lst_norm_vector[0] = {k: v for k, v in lst_norm_vector[0]}
 
     else:
@@ -180,8 +192,9 @@ def do_lst_norm_vector(krit=0, name=0, criteria=0, norm_vector=0, num_alt=0, g=0
 
         # сортування списку
         for c in range(criteria):
-            lst_norm_vector[c][c] = sorted(lst_norm_vector[c][c].items(),
-                                           key=operator.itemgetter(1), reverse=True)
+            lst_norm_vector[c][c] = sorted(
+                lst_norm_vector[c][c].items(), key=operator.itemgetter(1), reverse=True
+            )
             lst_norm_vector[c][c] = {k: v for k, v in lst_norm_vector[c][c]}
     return lst_norm_vector
 
@@ -193,14 +206,17 @@ def do_ranj(krit=0, lst_norm_vector=0, criteria=0, g=0):
         ctn = 0
         flag_key = None
 
-        ranj_str = ''
+        ranj_str = ""
         for key in lst_norm_vector[0].keys():
             if ctn == 0:
                 ranj_str += key
                 ctn += 1
             else:
-                ranj_str += ' > ' if lst_norm_vector[0][flag_key] > lst_norm_vector[0][
-                    key] else ' = '
+                ranj_str += (
+                    " > "
+                    if lst_norm_vector[0][flag_key] > lst_norm_vector[0][key]
+                    else " = "
+                )
                 ranj_str += key
             flag_key = key
         ranj.append(ranj_str)
@@ -211,13 +227,17 @@ def do_ranj(krit=0, lst_norm_vector=0, criteria=0, g=0):
 
         for c in range(criteria):
             ctn = 0
-            ranj_str = ''
+            ranj_str = ""
             for key in lst_norm_vector[c][c].keys():
                 if ctn == 0:
                     ranj_str += key
                     ctn += 1
                 else:
-                    ranj_str += ' > ' if lst_norm_vector[c][c][flag_key] > lst_norm_vector[c][c][key] else ' = '
+                    ranj_str += (
+                        " > "
+                        if lst_norm_vector[c][c][flag_key] > lst_norm_vector[c][c][key]
+                        else " = "
+                    )
                     ranj_str += key
                 flag_key = key
             ranj[c].append(ranj_str)
@@ -225,14 +245,18 @@ def do_ranj(krit=0, lst_norm_vector=0, criteria=0, g=0):
         ranj = []
         flag_key = 0
         ctn = 0
-        ranj_str = ''
+        ranj_str = ""
 
         for key in lst_norm_vector[0].keys():
             if ctn == 0:
                 ranj_str += key
                 ctn += 1
             else:
-                ranj_str += ' > ' if lst_norm_vector[0][flag_key] > lst_norm_vector[0][key] else ' = '
+                ranj_str += (
+                    " > "
+                    if lst_norm_vector[0][flag_key] > lst_norm_vector[0][key]
+                    else " = "
+                )
                 ranj_str += key
             flag_key = key
         ranj.append(ranj_str)
@@ -243,34 +267,57 @@ def do_ranj(krit=0, lst_norm_vector=0, criteria=0, g=0):
 def do_global_prior(norm_vector=0, norm_vector_alt=0, num_alt=0):
     global_prior = []
     for i in range(num_alt):
-        g_pr = sum(norm_vector[j] * norm_vector_alt[j][i] for j in range(len(norm_vector_alt)))
+        g_pr = sum(
+            norm_vector[j] * norm_vector_alt[j][i] for j in range(len(norm_vector_alt))
+        )
         global_prior.append(g_pr)
 
     return global_prior
 
 
 # дерево
-def generate_hierarchy_tree(name_criteria_tree, name_alternatives_tree, priority_vector, global_prior):
+
+
+def generate_hierarchy_tree(
+    name_criteria_tree, name_alternatives_tree, priority_vector, global_prior
+):
     dot = Digraph()
 
-    # Встановлення загальних атрибутів для вузлів та зв'язків
-    dot.attr('node', shape='oval', style='filled', fillcolor='lightblue')  # Форма вузла, стиль та колір заливки
-    dot.attr('edge', color='black', penwidth='1.0')  # Колір та товщина стрілки
+    # Цвета из CSS-переменных
+    color_background = "#0B0C10"
+    color_text = "#EDF5E1"
+    color_accent = "#66FCF1"
+    color_accent_dark = "#05386B"
+    color_link = "#8EE4AF"
 
-    # Додаємо вузли критеріїв та їх чисельні значення вектору пріоритету
-    for i, (criteria_name, priority) in enumerate(zip(name_criteria_tree, priority_vector)):
-        criteria_node_label = f'{criteria_name}\n({priority:.3f})'
-        dot.node(f'criteria_{criteria_name}', criteria_node_label, shape='box', style='filled',
-                 fillcolor='lightblue')
+    # Общие настройки
+    dot.attr(bgcolor=color_background)  # фон графа
+    dot.attr("node", fontcolor=color_background, fontname="Helvetica", fontsize="12")
+    dot.attr("edge", color=color_accent_dark, penwidth="1.2", arrowhead="vee")
 
-    # Додаємо вузли альтернатив та зв'язки з критеріями
-    for i, (alternative_name, priority) in enumerate(zip(name_alternatives_tree, global_prior)):
-        alternative_node_label = f'{alternative_name}\n({priority:.3f})'
-        dot.node(f'alternative_{alternative_name}', alternative_node_label, shape='ellipse', style='filled',
-                 fillcolor='lightyellow')  # Форма вузла, стиль та колір заливки
+    # Критерии
+    for criteria_name, priority in zip(name_criteria_tree, priority_vector):
+        label = f"{criteria_name}\n({priority:.3f})"
+        dot.node(
+            f"criteria_{criteria_name}",
+            label,
+            shape="box",
+            style="filled",
+            fillcolor=color_accent,
+        )
+
+    # Альтернативы
+    for alternative_name, global_priority in zip(name_alternatives_tree, global_prior):
+        label = f"{alternative_name}\n({global_priority:.3f})"
+        dot.node(
+            f"alternative_{alternative_name}",
+            label,
+            shape="ellipse",
+            style="filled",
+            fillcolor=color_link,
+        )
         for criteria_name in name_criteria_tree:
-            dot.edge(f'criteria_{criteria_name}', f'alternative_{alternative_name}', color='black', penwidth='1.0',
-                     arrowhead='open')  # Колір та товщина стрілки, вказівник
-    dot.attr(ranksep='1')
-    dot.render('static/img/hierarchy_tree', format='png', cleanup=True)
+            dot.edge(f"criteria_{criteria_name}", f"alternative_{alternative_name}")
 
+    dot.attr(ranksep="1")
+    dot.render("static/img/hierarchy_tree", format="png", cleanup=True)
